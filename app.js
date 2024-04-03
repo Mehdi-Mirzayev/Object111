b327ed33de02ed60cae1c7e4d643c55c08dfb67b
Mehdi-Mirzayev <161413559+Mehdi-Mirzayev@users.noreply.github.com>
2024-04-03 07:49:31 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+//!  1. mehsullarin ucuzdan bahaya siralanmasi
+
+// let mehsullar =[
+// {ad:"Toyota", qiymet:45000},
+// {ad:"Ford", qiymet:25000},
+// {ad:"Mercedes", qiymet:100000},
+// {ad:"Bmw", qiymet:12000}
+// ];
+
+// mehsullar.sort(function (a,b) {
+//   return a.qiymet-b.qiymet
+// })
+
+// console.log("Qiymete gore Siralanan Masinlar:");
+// for (let i = 0; i< mehsullar.length; i++) {
+// console.log(mehsullar[i].ad+":"+mehsullar[i].qiymet+" Azn");
+
+// }
+
+//!  2.Beş random eded yaradib arraya yigin, sonda ise onlarin cemini gosterin
+
+//   ??????
